40284c9cf38c470a285d35cbe0ce03dddfa2871f
Ashvat Bansal <ashvatb2@illinois.edu>
2018-01-31 16:35:09 -0600

    Maze Solved

--- src/main/java/SolveMaze.java
+++ src/main/java/SolveMaze.java
@@ -32,7 +32,7 @@ public class SolveMaze {
         /*
          * You should be able to solve a 10 x 10 maze in (far fewer than) 1000 steps.
          * Feel free to adjust this number if you experiment with other mazes.
-         * 
+         *
          */
         for (int step = 0; step < 1000; step++) {
 
